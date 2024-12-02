597bf60de0c7068c0fc85082de49254cea28b01e
K3r3n-D3V <161974086+K3r3n-D3V@users.noreply.github.com>
2024-12-02 21:45:52 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173', 
+    baseUrl: 'http://www.infostructure.com.s3-website-us-east-1.amazonaws.com/', 
     chromeWebSecurity: false,
   },
 });
